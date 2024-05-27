81dc14fb68696edf4d736cf20d12f31467b62cbd
AntonKurapov <AntonKurapov@x5.ru>
2024-05-27 12:37:19 +0300

    update nginx.html and output.sh

--- terraform/output.sh
+++ terraform/output.sh
@@ -8,6 +8,9 @@ KIBANA_INTERNAL_IP=$(terraform output -json internal_ip_address_kibana-server | 
 ELASTICSEARCH_INTERNAL_IP=$(terraform output -json internal_ip_address_elasticsearch-server | jq -r '.')
 PROMETHEUS_INTERNAL_IP=$(terraform output -json internal_ip_address_prometheus-server | jq -r '.')
 BASTION_EXTERNAL_IP=$(terraform output -json external_ip_address_bastion | jq -r '.')
+LB_EXTERNAL_IP=$(terraform output -json external_ip_address_web-alb | jq -r '.')
+GRAFANA_EXTERNAL_IP=$(terraform output -json external_ip_address_grafana-server | jq -r '.')
+KIBANA_EXTERNAL_IP=$(terraform output -json external_ip_address_kibana-server | jq -r '.')
 
 # Формируем inventory
 cat <<EOF > ../Ansible/inventory.ini
@@ -37,5 +40,22 @@ kibana-server ansible_host=$KIBANA_INTERNAL_IP
 [monitoring]
 prometheus-server ansible_host=$PROMETHEUS_INTERNAL_IP 
 grafana-server ansible_host=$GRAFANA_INTERNAL_IP 
+
+EOF
+
+# Добавляем в шаблон nginx внешние IP чтобы можно было их использовать для формирования страницы с информацией.
+
+cat <<EOF > ../Ansible/nginx_for_webservers/defaults/main.yml
+---
+# defaults file for nginx_for_webservers
+page_title: "Добро пожаловать на мой сайт"
+content_heading: "Информационный сайт по курсовой работе"
+content_body: "*ссылки на внешние сервисы, которые доступны из интернета"
+
+bastion_host_info: "bastion-host внешний IP :$BASTION_EXTERNAL_IP"
+lb_info: "load-balancer внешний IP :$LB_EXTERNAL_IP"
+
+grafana_info: "UI Grafana доступна по адресу: http://$GRAFANA_EXTERNAL_IP:3000"
+kibana_info: "UI Kibana доступна по адресу: http://$KIBANA_EXTERNAL_IP:5601"
 
 EOF
