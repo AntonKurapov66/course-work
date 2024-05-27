f663856faa5ea78d12ac2fc6eeea11f9f6dfdd65
AntonKurapov <AntonKurapov@x5.ru>
2024-05-27 18:33:42 +0300

    setings role all

--- terraform/output.sh
+++ terraform/output.sh
@@ -53,7 +53,15 @@ content_body: "*ссылки на внешние сервисы, которые 
 
 bastion_host_info: "bastion-host внешний IP :$BASTION_EXTERNAL_IP"
 
-grafana_info: "UI Grafana доступна по адресу: http://$GRAFANA_EXTERNAL_IP:3000"
-kibana_info: "UI Kibana доступна по адресу: http://$KIBANA_EXTERNAL_IP:5601"
+grafana_info: "http://$GRAFANA_EXTERNAL_IP:3000"
+kibana_info: "http://$KIBANA_EXTERNAL_IP:5601"
+
+EOF
+
+cat <<EOF > ../Ansible/filebeat/vars/main.yml
+---
+# vars file for filebeat
+kibana_host: "$KIBANA_EXTERNAL_IP:5601"
+elastic_host: "{{ hostvars['elasticsearch-server']['ansible_host'] }}:9200"
 
 EOF
