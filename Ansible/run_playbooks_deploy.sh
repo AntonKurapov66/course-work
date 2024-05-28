3e2ffb27c1cb0f58b388b21bd52c5333fc89c0f7
AntonKurapov <AntonKurapov@x5.ru>
2024-05-28 12:32:43 +0300

    add scripts run_playbooks_deploy

---
+++ Ansible/run_playbooks_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INVENTORY='inventory.ini'
+PLAYBOOKS=(
+  nginx_deploy.yml
+  prometheus_deploy.yml
+  node_exporter_deploy.yml
+#  nginx_log_ex_deploy.yml
+  grafana_deploy.yml
+  elasticsearch_deploy.yml
+  filebeat_deploy.yml
+  kibana_deploy.yml
+)
+
+for playbook in "${PLAYBOOKS[@]}"; do
+  echo "Running playbook: $playbook"
+  ansible-playbook "$playbook" -i "$INVENTORY"
+  
+  if [ "$?" -ne 0 ]; then
+    echo "Playbook $playbook failed. Stopping the script."
+    exit 1
+  fi
+  
+  echo "Playbook $playbook finished successfully."
+done
+
+echo "All playbooks have been executed successfully."
