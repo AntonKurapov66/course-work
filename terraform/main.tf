f6e7185040d79982d48c193893661691fe0cd092
AntonKurapov <AntonKurapov@x5.ru>
2024-05-28 17:26:26 +0300

    shapshot setings_v3

--- terraform/main.tf
+++ terraform/main.tf
@@ -465,7 +465,7 @@ resource "yandex_vpc_security_group" "public-load-balancer" {
 resource "yandex_compute_snapshot_schedule" "my-snapshot-disk" {
   name           = "my-snapshot-disk"
   schedule_policy {
-	expression = "17 35 * * *"
+	expression = "17 40 * * *"
   }
   snapshot_count = 1
   retention_period = "12h"
